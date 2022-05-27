e395d55fd542431ad87bddb9af17beeba163bc05
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-05-27 16:44:35 -0300

    novas funcionalidades adicionadas

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
-  // adicionar evento-paste
-  const input = document.querySelector("input[name='linha1']");
+  var allcontent = null
+  function pergunta() {
+    let LCD = document.getElementById('LCD')
+    let linha1 = document.querySelector("input[name='linha1']") /*Recebendo apeas o Imput, sem o valor*/
+    let linha2 = document.querySelector("input[name='linha2']") /*Recebendo apeas o Imput, sem o valor*/
+    let linha3 = document.querySelector("input[name='linha3']") /*Recebendo apeas o Imput, sem o valor*/
+    linha1.value = linha1.value.replace(/[^0-9A-Za-z!%()_\-.,?\/ ]/g, "").toUpperCase(); /*TRATANDO IMPUT PARA PROIBIR CARACTERES ESPECIAIS.*/
+    linha2.value = linha2.value.replace(/[^0-9A-Za-z!%()_\-.,?\/ ]/g, "").toUpperCase(); /*TRATANDO IMPUT PARA PROIBIR CARACTERES ESPECIAIS.*/
+    linha3.value = linha3.value.replace(/[^0-9A-Za-z!%()_\-.,?\/ ]/g, "").toUpperCase(); /*TRATANDO IMPUT PARA PROIBIR CARACTERES ESPECIAIS.*/
+    allcontent = LCD.innerHTML = linha1.value + '<br>' + linha2.value + '<br>' + linha3.value
+  }
 
-  input.addEventListener('paste', function () {
-    const regex = new RegExp("^[0-9a-zA-Z\b]+$");
-   const self = this;
-  
-  // precisa ser colocado
-  setTimeout(function () {
+  function carregar() {
+    var input = document.querySelector('input[name=novoModelo]');
+    var btn = document.querySelector('input[name=btnAddModelo]');
+    var combo = document.querySelector('select[name=modelo]');
 
-    const text = self.value;
-
-    if (!regex.test(text)) {
-      self.value = "";
+    btn.addEventListener('click', function (e) {
+      if (input.value == "") {
+        alert("Informe um modelo.");
+        return false;
+      }
+      var retorno = true;
+      Array.from(combo.options).forEach(function (option_element) {
+        let option_value = option_element.value;
+        if (option_value == input.value) {
+          alert("Modelo já adicionado.");
+          input.value = "";
+          retorno = false;
+          return false;
+        }
+      });
+      if (retorno) {
+        var opt = document.createElement('option');
+        opt.value = input.value;
+        opt.innerHTML = input.value;
+        combo.appendChild(opt);
+        input.value = "";
+      }
+    });
+  }
+  function adicionarpergunta() {
+    if (allcontent == null) {
+      alert("Pergunta em Branco")
+    } else {
+      let divChecklist = document.getElementById('checklist')
+    let miniaturaPO = document.createElement("div")
+    miniaturaPO.setAttribute("class", "miniPO")
+     let miniaturaLCD = document.createElement("div")
+     miniaturaLCD.setAttribute("class", "LCD")
+    miniaturaLCD.innerHTML = allcontent
+    miniaturaPO.appendChild(miniaturaLCD)
+    divChecklist.appendChild(miniaturaPO)
     }
-  }, 10);
-});
-
-function mudando() {
-  let LCD = document.getElementById('LCD')
-  let linha1 = document.querySelector("input[name='linha1']").value /*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
-  let linha2 = document.querySelector("input[name='linha2']").value /*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
-  let linha3 = document.querySelector("input[name='linha3']").value /*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
-  LCD.innerText = linha1.toUpperCase() + '\n' + linha2.toUpperCase() + '\n' + linha3.toUpperCase()
-}
+  }
