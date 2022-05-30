7f48ddb1110fa3d3b14b5d2f16d52e5b894d30d5
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-05-30 16:53:49 -0300

    novas funcionalidades

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
   var allcontent = null
+  var linha1 = ""
+  var linha2 = ""
+  var linha3 = ""
+  var LCD = ""
+
   function pergunta() {
-    let LCD = document.getElementById('LCD')
-    let linha1 = document.querySelector("input[name='linha1']") /*Recebendo apeas o Imput, sem o valor*/
-    let linha2 = document.querySelector("input[name='linha2']") /*Recebendo apeas o Imput, sem o valor*/
-    let linha3 = document.querySelector("input[name='linha3']") /*Recebendo apeas o Imput, sem o valor*/
+    LCD = document.getElementById('LCD')
+    linha1 = document.querySelector("input[name='linha1']") /*Recebendo apeas o Imput, sem o valor*/
+    linha2 = document.querySelector("input[name='linha2']") /*Recebendo apeas o Imput, sem o valor*/
+    linha3 = document.querySelector("input[name='linha3']") /*Recebendo apeas o Imput, sem o valor*/
     linha1.value = linha1.value.replace(/[^0-9A-Za-z!%()_\-.,?\/ ]/g, "").toUpperCase(); /*TRATANDO IMPUT PARA PROIBIR CARACTERES ESPECIAIS.*/
     linha2.value = linha2.value.replace(/[^0-9A-Za-z!%()_\-.,?\/ ]/g, "").toUpperCase(); /*TRATANDO IMPUT PARA PROIBIR CARACTERES ESPECIAIS.*/
     linha3.value = linha3.value.replace(/[^0-9A-Za-z!%()_\-.,?\/ ]/g, "").toUpperCase(); /*TRATANDO IMPUT PARA PROIBIR CARACTERES ESPECIAIS.*/
@@ -14,7 +19,6 @@
     var input = document.querySelector('input[name=novoModelo]');
     var btn = document.querySelector('input[name=btnAddModelo]');
     var combo = document.querySelector('select[name=modelo]');
-
     btn.addEventListener('click', function (e) {
       if (input.value == "") {
         alert("Informe um modelo.");
@@ -39,22 +43,58 @@
       }
     });
   }
+
   function adicionarpergunta() {
-    if (allcontent == null) {
-      alert("Pergunta em Branco")
+    if (allcontent == null || linha1.value == "") {
+      alert("Digite uma pergunta começando da 1ª linha.")
     } else {
       let divChecklist = document.getElementById('checklist')
-    let miniaturaPO = document.createElement("div")
-    miniaturaPO.setAttribute("class", "miniPO")
-     let miniaturaLCD = document.createElement("div")
-     miniaturaLCD.setAttribute("class", "LCD")
-    miniaturaLCD.innerHTML = allcontent
-    miniaturaPO.appendChild(miniaturaLCD)
-    divChecklist.appendChild(miniaturaPO)
+      let miniaturaPO = document.createElement("div")
+      let miniaturaLCD = document.createElement("div")
+      var critica = document.createElement("span")
+      var bloqueia = document.getElementById('bloqueia')
+      let contador = document.createElement("p")
+      contador.setAttribute("id","contador")
+      let posicaopergunta = divChecklist.children.length + 1
 
-    let LCD = document.getElementById('LCD')
-    LCD.innerHTML =""
-    let linha = document.querySelector("input")
+      miniaturaPO.setAttribute("class", "miniPO")
+      miniaturaLCD.setAttribute("class", "LCD")
+      miniaturaLCD.innerHTML = allcontent
+      contador.innerText = "Pergunta " + posicaopergunta
+      miniaturaPO.appendChild(miniaturaLCD)
+      divChecklist.appendChild(miniaturaPO)
+      critica.setAttribute("id", "critica")
+      miniaturaPO.appendChild(contador)
+      
+      if (bloqueia.checked) {
+        critica.innerHTML = "ATENÇÃO Pergunta CRÍTICA"
+        miniaturaPO.appendChild(critica)
+      }
+      
+      let botaoremover = document.createElement("button")
+      botaoremover.type = "button"
+      botaoremover.innerText = "Remover Pergunta"
+      botaoremover.setAttribute("onclick", "remover(this)")
+
+      miniaturaPO.appendChild(botaoremover)
+    }
+    LCD.innerHTML = ""
     linha1.value = ""
+    linha2.value = ""
+    linha3.value = ""
+    allcontent = null
+  }
+
+  function remover(button) {
+    let checklistremover = button.parentNode
+    document.getElementById("checklist").removeChild(checklistremover)
+
+    let divChecklist = document.getElementById('checklist')
+    let posicaopergunta = divChecklist.children.length
+
+    for (let index = 1; index < posicaopergunta; index++) {
+      document.getElementById("contador").innerHTML = "Posição " + index
+      
     }
   }
+  
